63d1ac54d0c00ec55956240decdc04f8399781c5
zhangcz <214426857@qq.com>
2024-02-21 16:52:45 +0800

    button初始化

--- orange-ui-1/src/components/ButtonDemo.vue
+++ orange-ui-1/src/components/ButtonDemo.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
-       <Button>
-        你好
-       </Button>
+        <Button @click="onClick" @mouseover="onClick" size="big">
+            你好
+        </Button>
     </div>
 </template>
   
 <script setup lang="ts">
 import Button from '../lib/Button.vue'
+
+const onClick = () => {
+    console.log('hi');
+}
+
 </script>
   
 <style lang="scss" scoped></style>
--- orange-ui-1/src/lib/Button.vue
+++ orange-ui-1/src/lib/Button.vue
@@ -1,13 +1,27 @@
 <template>
-    <button>
-        <slot></slot>
-    </button>
+    <div class="wrapper">
+        <button v-bind="$attrs">
+            <slot></slot>
+        </button>
+    </div>
 </template>
   
-<script setup lang="ts">
-
+<script lang="ts">
+export default {
+    inheritAttrs: false,
+    setup(props, context) {
+        console.log(props);
+        console.log(context.attrs);
+        const { size, ...rest } = context.attrs
+        return { size, rest }
+    }
+}
 
 </script>
   
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.wrapper {
+    border: 1px solid red;
+}
+</style>
   
