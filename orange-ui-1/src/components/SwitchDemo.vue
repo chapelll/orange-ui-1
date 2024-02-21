9a01377c89ee8dccc0470540e6847f09ac1d6b15
zhangcz <214426857@qq.com>
2024-02-21 13:23:32 +0800

    switch组件添加切换动画

--- orange-ui-1/src/components/SwitchDemo.vue
+++ orange-ui-1/src/components/SwitchDemo.vue
@@ -1,110 +1,19 @@
 <template>
     <div>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
-        <p>Switch 的文档</p>
+        <Switch></Switch>
     </div>
 </template>
   
-<script setup lang="ts">
+<script lang="ts">
+import Switch from '../lib/Switch.vue'
+export default {
+    components: {
+        Switch
+    }
+}
+
 </script>
+
   
 <style lang="scss" scoped></style>
   
---
+++ orange-ui-1/src/lib/Switch.vue
@@ -0,0 +1,47 @@
+<template>
+    <button @click="toggle" :class="{ 'checked': checked }"> <span></span> </button>
+</template>
+  
+<script setup lang="ts">
+import { ref } from 'vue'
+const checked = ref(true)
+
+const toggle = () => {
+    checked.value = !checked.value
+}
+
+</script>
+  
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+
+button {
+    height: $h;
+    width: $h*2;
+    border: none;
+    background: grey;
+    border-radius: $h/2;
+    position: relative;
+}
+
+button.checked {
+    background: blue;
+}
+
+span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: all 250ms;
+}
+
+button.checked>span {
+    left: calc(100% - #{$h2} - 2px);
+}
+</style>
+  
