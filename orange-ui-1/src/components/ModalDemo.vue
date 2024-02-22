2b885fc08fddad0fe1b90250290de8b27b5875c6
zhangcz <214426857@qq.com>
2024-02-22 17:17:20 +0800

    创建modal组件

---
+++ orange-ui-1/src/components/ModalDemo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div>Modal示例</div>
+        <div>
+            <h1>示例1</h1>
+            <div>
+                <Button @click="toggle">切换</Button>
+                <Modal :visible="visible"></Modal>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script setup lang="ts">
+import Modal from '../lib/Modal.vue'
+import Button from '../lib/Button.vue'
+import { ref } from 'vue';
+
+const visible = ref(false)
+const toggle = () => {
+    visible.value = !visible.value
+}
+
+</script>
+  
+<style lang="scss" scoped></style>
+  
--- orange-ui-1/src/router.ts
+++ orange-ui-1/src/router.ts
@@ -4,7 +4,7 @@ import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
 import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
-import DialogDemo from './components/DialogDemo.vue'
+import ModalDemo from './components/ModalDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import DocDemo from './components/DocDemo.vue'
 
@@ -27,8 +27,8 @@ export const router = createRouter({
             path: 'button',
             component: ButtonDemo,
         }, {
-            path: 'dialog',
-            component: DialogDemo,
+            path: 'modal',
+            component: ModalDemo,
         }, {
             path: 'tabs',
             component: TabsDemo,
