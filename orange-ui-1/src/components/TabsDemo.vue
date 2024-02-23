f67eb4c0b5f554b459c6fa9b05399d2615d4fe15
zhangcz <214426857@qq.com>
2024-02-23 17:16:06 +0800

    初始化tabs组件

--- orange-ui-1/src/components/TabsDemo.vue
+++ orange-ui-1/src/components/TabsDemo.vue
@@ -1,10 +1,19 @@
 <template>
+    <div>Tabs示例</div>
     <div>
-        <p>Tbas 的文档</p>
+        <h1>示例1</h1>
+        <div>
+            <Tabs selected="导航一">
+                <Tab title="导航一">选项一</Tab>
+                <Tab title="导航二">选项二</Tab>
+            </Tabs>
+        </div>
     </div>
 </template>
   
 <script setup lang="ts">
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
 </script>
   
 <style lang="scss" scoped></style>
---
+++ orange-ui-1/src/lib/Tabs.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="orange-tabs">
+        <div class="orange-tabs-nav">
+            <div class="orange-tabs-nav-item" :class="{ 'selected': t === props.selected }"
+                v-for="(t, index) in titles" :key="index">{{ t }}</div>
+        </div>
+        <div class="orange-tabs-content">
+            <component class="orange-tabs-content-item" v-for="(c, index) in defaults" :is="c" :key="index" />
+        </div>
+    </div>
+</template>
+  
+<script setup lang="ts">
+import { useSlots } from 'vue'
+import Tab from '../lib/Tab.vue'
+
+const props = defineProps({
+    selected: {
+        type: String,
+    }
+})
+
+const defaults = useSlots().default()
+defaults.forEach((item, index) => {
+    if (item.type !== Tab) {
+        throw new Error(`Tabs下的第${index + 1}个子组件不是Tab!`)
+    }
+})
+const titles = defaults.map((item) => {
+    return item.props.title
+})
+
+
+
+</script>
+  
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.orange-tabs {
+    &-nav {
+        display: flex;
+        color: $color;
+        border-bottom: 1px solid $border-color;
+
+        &-item {
+            padding: 8px 0;
+            margin: 0 16px;
+            cursor: pointer;
+
+            &:first-child {
+                margin-left: 0;
+            }
+
+            &.selected {
+                color: $blue;
+            }
+        }
+    }
+
+    &-content {
+        padding: 8px 0;
+    }
+}
+</style>
+  
